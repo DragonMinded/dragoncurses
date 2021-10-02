28e6ee320da67c0db44a9adb4e1b1d7ce7eac56b
Jennifer Taylor <dragonminded@dragonminded.com>
2021-10-02 18:57:18 +0000

    Attempt to auto-install windows-curses on windows environments.

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 from setuptools import setup  # pyre-ignore
 
+
+def format_req(req: str) -> str:
+    if ";" not in req:
+        return req
+
+    req, extra = req.split(";", 1)
+    req = req.strip()
+    extra = extra.strip()
+
+    if "sys.platform" in extra and "==" in extra:
+        platform = None
+        if "win32" in extra:
+            platform = "Windows"
+
+        if platform is not None:
+            return f"{req}; platform_system=='{platform}'"
+
+    raise Exception(f"Don't know how to format {req}!")
+
+
 setup(
     name='dragoncurses',
     version='0.1',
@@ -12,7 +32,7 @@ setup(
         'dragoncurses',
     ],
     install_requires=[
-        req for req in open('requirements.txt').read().split('\n') if len(req) > 0
+        format_req(req) for req in open('requirements.txt').read().split('\n') if len(req) > 0
     ],
     include_package_data=True,
     zip_safe=False,
